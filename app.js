c50958ac1ff5deb6cf5a72245ad2bd2496de5569
himanshu <himanshumehta1114@gmail.com>
2017-09-24 17:47:04 +0530

    Fixed buttons

---
+++ app.js
@@ -0,0 +1,23 @@
+	var acc = "";
+	var getExp = function(button){
+		acc += button;
+		console.log(button);
+		$('#display').append(button);
+	};
+$(document).ready(function(){
+	$('#1').click(function(){getExp(1);});
+	$('#2').click(function(){getExp(2);});	
+	$('#3').click(function(){getExp(3);});
+	$('#4').click(function(){getExp(4);});
+	$('#5').click(function(){getExp(5);});
+	$('#6').click(function(){getExp(6);});
+	$('#7').click(function(){getExp(7);});
+	$('#8').click(function(){getExp(8);});
+	$('#9').click(function(){getExp(9);});
+	$('#0').click(function(){getExp(0);});
+	$('#dot').click(function(){getExp('.');});
+	$('#sum').click(function(){getExp('+');});
+	$('#sub').click(function(){getExp('-');});
+	$('#mul').click(function(){getExp('*');});
+	$('#div').click(function(){getExp('/');});
+});
